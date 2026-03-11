74bafa9107cbe72292c957b1b39ec853a551b3fd
codee413 <kjaswanth151@gmail.com>
2026-03-11 15:25:37 +0530

    UC8: Added implementation code for OOPS Banner App

---
+++ OOPSBannerAppUC8.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+import java.util.Map;
+public class OOPSBannerAppUC8 {
+    private static Map<Character, String[]> patternMap = new HashMap<>();
+    public static void initializePatterns() {
+        patternMap.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+        patternMap.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        });
+        patternMap.put('S', new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        });
+    }
+    public static void renderBanner(String word) {
+        int height = patternMap.get(word.charAt(0)).length;
+        for (int row = 0; row < height; row++) {
+            StringBuilder line = new StringBuilder();
+            for (char ch : word.toCharArray()) {
+                String[] pattern = patternMap.get(ch);
+                if (pattern != null) {
+                    line.append(pattern[row]).append("  ");
+                } else {
+                    line.append("     ");
+                }
+            }
+            System.out.println(line);
+        }
+    }
+    public static void main(String[] args) {
+        initializePatterns();
+        String bannerWord = "OOPS";
+        renderBanner(bannerWord);
+    }
+}
