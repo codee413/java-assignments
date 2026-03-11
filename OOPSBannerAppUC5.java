cbdbdd88c480d9f86a47d40ee34f6c81edc19352
codee413 <kjaswanth151@gmail.com>
2026-03-11 15:11:45 +0530

    UC5: Implement banner using array initialization with String.join()

--- OOPSBannerAppUC5.java
+++ OOPSBannerAppUC5.java
@@ -1,16 +1,11 @@
 public class OOPSBannerAppUC5 {
-
     public static void main(String[] args) {
-
-        
         String[] banner = {
-            String.join("", "*", "*", "*", "*", "*", "*", "*", "*", "*"),
-            String.join("", "*", " ", "O", " ", "O", " ", "P", " ", "S", " ", "*"),
-            String.join("", "*", " ", "O", " ", "O", " ", "P", " ", "S", " ", "*"),
-            String.join("", "*", " ", "O", " ", "O", " ", "P", " ", "S", " ", "*"),
-            String.join("", "*", " ", "O", " ", "O", " ", "P", " ", "S", " ", "*"),
-            String.join("", "*", " ", "O", " ", "O", " ", "P", " ", "S", " ", "*"),
-            String.join("", "*", "*", "*", "*", "*", "*", "*", "*", "*")
+                String.join("  ", "****", "\t", "****", "\t", "*****", "\t", "****"),
+                String.join("  ", "*  *", "\t", "*  *", "\t", "*   *", "\t", "*"),
+                String.join("  ", "*  *", "\t", "*  *", "\t", "*****", "\t", "****"),
+                String.join("  ", "*  *", "\t", "*  *", "\t", "*", "\t\t", "   *"),
+                String.join("  ", "****", "\t", "****", "\t", "*", "\t\t", "****")
         };
 
         for (String line : banner) {
